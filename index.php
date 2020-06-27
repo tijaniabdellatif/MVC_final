a8f8ed7b10727c5fc2d581e1da418d85f0adf194
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-06-27 16:43:17 +0100

    entry_file_index

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//definition de la page courante
+if(isset($_GET['page']) AND !empty($_GET['page'])){
+
+   $page = trim(strtolower($_GET['page'])); //HOME
+
+}
+else {
+
+    $page = 'home';
+}
+
+//array contenant toutes les pages
+$allpages = scandir('controllers/');
+var_dump($allpages);
+
+if(in_array($page.'_controller.php',$allpages)){
+
+    //inclusion de la page
+    include_once 'models/'.$page.'_model.php';
+    include_once 'controllers/' . $page . '_controller.php';
+    include_once 'views/' . $page . '_view.php';
+}else {
+    //return error
+    echo 'Erreur 404';
+
+}
