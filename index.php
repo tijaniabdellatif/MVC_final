ffcf4e52a66a02433e5dea10d12d31c2cd04220b
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-06-28 16:35:25 +0100

    _functions

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 include_once '_config/config.php';
 include_once '_config/db.php';
-var_dump($db);
+include_once '_functions/functions.php';
+
+// debug(['hello','world']);
+// exit;
+echo '<h1>hello</h1>';
+ echo debug(str_secur('<h1>hello</h1>'));
 
 //definition de la page courante
 if(isset($_GET['page']) AND !empty($_GET['page'])){
